40078828a95f3b0850f659f9e4ff3357bb9cf918
Princessbolade <mobolade.akinlua@outlook.com>
2021-02-22 16:16:49 +0100

    Header Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./pages/homepage/hompeage";
 import ShopPage from "./pages/homepage/shop/shop";
+import Header from "./components/header/header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopPage} />
