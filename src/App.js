5ab51f4f67fc9dcebe823b8330b6413f403a5376
Princessbolade <mobolade.akinlua@outlook.com>
2021-02-22 11:09:54 +0100

    Updated menu-items to navigate with router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Homepage } from "./pages/homepage/hompeage";
+
+const BeaniesPage = () => (
+  <div>
+    <h1>Beanies PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={BeaniesPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/directory/directory.jsx
+++ src/components/menu-item/directory/directory.jsx
@@ -15,18 +15,21 @@ class Directory extends React.Component {
           imageUrl:
             "https://images.unsplash.com/photo-1598539961898-eaf3eab30294?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
           id: 1,
+          linkUrl: "beanies",
         },
         {
           title: "Jackets",
           imageUrl:
             "https://images.unsplash.com/photo-1601924994987-69e26d50dc26?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1950&q=80",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "Sneakers",
           imageUrl:
             "https://images.unsplash.com/photo-1612821745127-53855be9cbd1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "Womens",
@@ -34,6 +37,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1608915812295-417351ccf39b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "Mens",
@@ -41,6 +45,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1486645725491-57c86b563b91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
