33f06c5bc60a5e05ef2455a3a1b7c36705d4b94b
Princessbolade <mobolade.akinlua@outlook.com>
2021-02-27 01:25:21 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -12,7 +12,7 @@ const CartIcon = ({ toggleCartHidden }) => (
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
+  toggleCartHidden: () => dispatch(toggleCartHidden),
 });
 
 export default connect(mapDispatchToProps)(CartIcon);
